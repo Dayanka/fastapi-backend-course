2aa073b8791a657acc9af38825c53850cb7e1574
dayana <dayaankaaa@gmail.com>
2025-02-17 17:25:47 +0300

    дз по ооп

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,44 @@
+
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/tasks")
+tasks = []
+
+class Task(BaseModel):
+    title: str
+    status: str
+
+@app.get('/tasks', response_model=List[Task])
 def get_tasks():
-    pass
+    return tasks
 
-@app.post("/tasks")
-def create_task(task):
-    pass
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+@app.post('/task', response_model=Task)
+def create_task(task: Task):
+    task_id = len(tasks) +1
+    new_task = task.dict()
+    new_task['id'] = task_id
+    tasks.append(new_task)
+    return new_task
 
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    pass
+@app.put('/tasks/{task_id}', response_model=Task)
+def update_task(task_id: int, task: Task):
+    for t in tasks:
+        if t['id']==task_id:
+            t['title'] = task.title
+            t['status'] = task.status
+            return t
+    return {'error': 'task not found'}
+
+@app.delete('/tasks/{task_id}')
+def delete_task(task_id:int):
+    global tasks
+    tasks = [t for t in tasks if t['id'] != task_id]
+    return {'message': 'task deleted successfully'}
+
+
+
